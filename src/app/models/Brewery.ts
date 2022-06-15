0718f347069b7863145073273ac88c95379dfd31
Shawn Cannon <scanno16@ford.com>
2022-06-15 07:58:23 -0600

    add link feature

--- src/app/models/Brewery.ts
+++ src/app/models/Brewery.ts
@@ -1,4 +1,12 @@
  export type Brewery = {
   name: string;
   imageUrl: string;
+  description: string;
+  websiteUrl: string;
+  ipaOption: string;
+  sourOption: string;
+  stoutOption: string;
+  amenityOne: string;
+  amenityTwo: string;
+
 }
