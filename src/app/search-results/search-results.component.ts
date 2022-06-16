fb5fc21cc271ce26deceaf020563a4d6bb77c835
Shawn Cannon <scanno16@ford.com>
2022-06-16 12:21:18 -0600

    update search

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BreweryService } from '../brewery.service';
 import { Brewery } from '../models/Brewery';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
@@ -15,15 +15,11 @@ export class SearchResultsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private breweryService: BreweryService) { }
 
-
-
-
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.searchName = decodeURIComponent(params["search"])
       this.breweryService.search(this.searchName).subscribe(response => {
         this.results = response.breweries
-        console.log(this.results)
       })
     }
     )
